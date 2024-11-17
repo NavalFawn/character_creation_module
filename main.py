500b45fa41fb6956d8a5222be4b6fb5abc13aab1
Артём Павелков <pavelkov96@mail.ru>
2024-11-17 19:05:22 +0300

    Новый апдейт

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def defence(char_name, char_class):
     if char_class == 'mage':
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
-        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+        return f'{char_name} блокировал {10 + randint(2, 5)} урона'
 def special(char_name, char_class):
-    if char_class == 'warrior': 
+    if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
     if char_class == 'mage':
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
